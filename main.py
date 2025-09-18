2f99738be3666d15927dbe6525f503c1e502dcd3
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-18 11:49:36 +0100

    make an export command exist

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import click
+import requests
+
 HACK_CLUB_WORKSPACE = "T0266FRGM"
 
 
@@ -5,9 +8,15 @@ def canvas_html_url(canvas_id: str) -> str:
     return f"https://files.slack.com/files-pri/{HACK_CLUB_WORKSPACE}-{canvas_id}/canvas"
 
 
-def main():
-    print("Hello from slack-canvas-exporter!")
+@click.command()
+@click.argument("canvas_id")
+def export(canvas_id: str):
+    url = canvas_html_url(canvas_id)
+    print(f"Canvas URL: {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+    file_name = f"{canvas_id}.html"
 
 
 if __name__ == "__main__":
-    main()
+    export()
